4ab60d527c3cfe88f7b04be886f98841ec62a42d
Enoch <enochagbloe16@gmail.com>
2025-06-19 16:01:31 +0100

    Collection model implemented

---
+++ database/collection.model.ts
@@ -0,0 +1,13 @@
+import { model, models, Schema, Types } from "mongoose";
+
+export interface ICollection {
+    author: Types.ObjectId,
+    question: Types.ObjectId, 
+}
+const  CollectionSchema = new Schema({
+    author: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    question: { type: Schema.Types.ObjectId, ref: "Question", required: true },
+})
+
+const Collection = models?.collection || model<ICollection>("collection", CollectionSchema);
+export default Collection;
